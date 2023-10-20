b2d1edcca5660c8d99bfbdc9bffbe7b5946e1a1d
usman_sol <usman.liaqut@unitedsol.net>
2023-10-20 15:30:38 +0500

    new pages added

--- pages/api/getproducts.js
+++ pages/api/getproducts.js
@@ -5,6 +5,21 @@ import connectDb from "@/middleware/moongose"
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const  handler=async(req,res)=>{
     let products=await product.find()
+    let tshirts={}
+    for(let item of products)
+    {
+        if(item.title in tshirts)
+        {}
+        else
+        {
+            tshirts[item.title]=JSON.parse(JSON.stringify(item))
+            if(item.availableQty>0)
+            {
+                tshirts[item.title].color=[item.color]
+                tshirts[item.title].size=[item.size]
+            }
+        }
+    }
     res.status(200).json({ products })
 }
 
